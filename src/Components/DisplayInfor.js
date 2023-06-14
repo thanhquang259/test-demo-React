822879c392eda2a1333c633a4ba5cd2cdb42fc84
Nguyen Thanh Quang <nquang2591721@gmail.com>
2023-06-14 15:56:17 +0700

    video 27

--- src/Components/DisplayInfor.js
+++ src/Components/DisplayInfor.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './DisplayInfor.scss';
 class DisplayInfor extends React.Component {
     render() {
         //destructuring array/object
@@ -7,16 +7,19 @@ class DisplayInfor extends React.Component {
         //const listUsers = this.props.listUsers;
         //props => viết tắt của properties truyền dữ liệu từ cha sang con
         return (
-            <div>
+            <div className="DisplayInfor">
                 {listUsers.map((user) => {  // user là tên tự đặt
                     return (
-                        <div key={user.id}>  dùng key để phân biệt các div
-                            <div>My name's {user.name}</div>
+                        <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                            <div >My name's {user.name}</div>
+                            {/* style={{ color: 'pink', paddingTop: '50px' }} */}
                             <div>My age's {user.age} </div>
                             <hr />
                         </div>
                     )
                 })}
+
+
 
 
             </div>
