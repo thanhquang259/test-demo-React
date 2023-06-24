83190a68c3d466f4145b98d1738ee4e2c9888272
Nguyen Thanh Quang <nquang2591721@gmail.com>
2023-06-24 14:33:01 +0700

    video 52

--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -1,7 +1,9 @@
+import videoHomepage from '../../assets/videoHomepage.mp4'
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            Home page components
+            <video width='750' height='500' autoPlay muted loop>
+                <source src={videoHomepage} type='video/mp4' /></video>
         </div>
     )
 }
