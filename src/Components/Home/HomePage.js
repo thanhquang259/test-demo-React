cac26f239105914ec47702fdc1f8556d36c68228
Nguyen Thanh Quang <nquang2591721@gmail.com>
2023-06-26 15:23:14 +0700

    video 54

--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -4,7 +4,21 @@ const HomePage = (props) => {
         <div className="homepage-container">
             <video width='750' height='500' autoPlay muted loop>
                 <source src={videoHomepage} type='video/mp4' /></video>
+            <div className='homepage-content'>
+                <div className='title-1'>Forms
+                    <br></br>
+                    that break
+                    <br></br>
+                    the norm</div>
+                <div className='title-2'>Get more data—like signups, feedback, and anything else—with forms designed to be refreshingly different.</div>
+                <div className='title-3'>
+                    <button>
+                        Get started- it's free
+                    </button>
+                </div>
+            </div>
         </div>
+
     )
 }
 export default HomePage;
